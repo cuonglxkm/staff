fba9fbce89394af606b41c44b78f4746a78902f2
Your Name <cuonglxkm@gmail.com>
2019-09-29 03:37:30 +0700

    'Dem_hom_t7!!'

--- src/app/staff/staff.component.ts
+++ src/app/staff/staff.component.ts
@@ -1,60 +1,73 @@
-import { Component, OnInit } from '@angular/core';
-import { Staff } from './staff';
-import { NzModalService } from 'ng-zorro-antd/modal';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { NzFormModule } from 'ng-zorro-antd/form';
-import { NgForm } from '@angular/forms'
+import { Component, OnInit } from "@angular/core";
+import { Staff } from "./staff";
+import { NzModalService } from "ng-zorro-antd/modal";
 
 @Component({
-  selector: 'app-staff',
-  templateUrl: './staff.component.html',
-  styleUrls: ['./staff.component.scss']
+  selector: "app-staff",
+  templateUrl: "./staff.component.html",
+  styleUrls: ["./staff.component.scss"]
 })
-
 export class StaffComponent implements OnInit {
-
+  constructor(private modalService: NzModalService) {}
   listStaff: Staff[] = [
-    new Staff('nv01', 'Phạm Văn Cương', 'cuonglxkm@gmail.com', '0961080526', 'Hoàng Mai, Hà Nội'),
-    new Staff('nv02', 'Hải tc', 'haitc@gmail.com', '0303030303', 'Minh Khai, Hà Nội'),
-    new Staff('nv03', 'Sơn', 'son@gmail.com', '13123123312', 'Hoàng Mai, Hà Nội'),
-    new Staff('nv04', 'Thái', 'tuan@gmail.com', '123123', 'Hoàng Mai, Hà Nội'),
-    new Staff('nv05', 'Tuấn', 'tuan@gmail.com', '12213123', 'Hoàng Mai, Hà Nội')
+    new Staff(
+      "nv01",
+      "Phạm Văn Cương",
+      "cuonglxkm@gmail.com",
+      "0961080526",
+      "Hoàng Mai, Hà Nội"
+    ),
+    new Staff(
+      "nv02",
+      "Hải tc",
+      "haitc@gmail.com",
+      "0303030303",
+      "Minh Khai, Hà Nội"
+    ),
+    new Staff(
+      "nv03",
+      "Sơn",
+      "son@gmail.com",
+      "13123123312",
+      "Hoàng Mai, Hà Nội"
+    ),
+    new Staff("nv04", "Thái", "tuan@gmail.com", "123123", "Hoàng Mai, Hà Nội"),
+    new Staff("nv05", "Tuấn", "tuan@gmail.com", "12213123", "Hoàng Mai, Hà Nội")
   ];
   isSearch = false;
-  searchString = '';
+  searchString = "";
   listSearch = [];
   isVisible = false;
-  newStaff: Staff;
-
-  constructor(private modalService: NzModalService) { }
+  newStaff: Staff = new Staff("", "", "", "", "");
+  errors = [];
+  style = new Staff("", "", "", "", "");
 
   ngOnInit() {
     this.listSearch = JSON.parse(JSON.stringify(this.listStaff));
   }
 
-
-
   onClick() {
-    if (this.searchString !== '') {
-      let data = this.listStaff.filter(x =>
-        x.id.indexOf(this.searchString) !== -1 ||
-        x.name.indexOf(this.searchString) !== -1 ||
-        x.email.indexOf(this.searchString) !== -1 ||
-        x.phone.indexOf(this.searchString) !== -1);
+    if (this.searchString !== "") {
+      const data = this.listStaff.filter(
+        x =>
+          x.id.indexOf(this.searchString) !== -1 ||
+          x.name.indexOf(this.searchString) !== -1 ||
+          x.email.indexOf(this.searchString) !== -1 ||
+          x.phone.indexOf(this.searchString) !== -1
+      );
       this.listSearch = JSON.parse(JSON.stringify(data));
-    }
-    else {
+    } else {
       this.listSearch = JSON.parse(JSON.stringify(this.listStaff));
     }
-    this.searchString = '';
+    this.searchString = "";
   }
 
   xoa(id: any) {
     this.modalService.confirm({
-      nzTitle: '<i>Do you Want to delete these items?</i>',
-      nzContent: '<b>Some descriptions</b>',
-      nzOkText: 'Ok',
-      nzCancelText: 'Cancel',
+      nzTitle: "<i>Do you Want to delete these items?</i>",
+      nzContent: "<b>Some descriptions</b>",
+      nzOkText: "Ok",
+      nzCancelText: "Cancel",
       nzOnOk: () => {
         let index = this.listSearch.findIndex(x => x.id === id);
         if (index !== -1) {
@@ -66,52 +79,83 @@ export class StaffComponent implements OnInit {
 
   showModal(): void {
     this.isVisible = true;
+    this.errors = [];
   }
 
-  valid(staff: Staff) {
-    var erro = [];
-    if (staff.id == "")
-      erro.push({ id: "Mã nhân viên không được để trống!" });
-    if (staff.name == "") {
-      erro.push({ name: "Tên nhân viên không được để trống!" });
-    }
-    if (staff.email == "") {
-      erro.push({ email: "Email không được để trống!" });
-    }
-    else {
-      let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if (!re.test(String(staff.email).toLowerCase())) {
-        erro.push({ email: "Địa chỉ email sai cú pháp" })
-      }
-    }
-    if (staff.phone == "") {
-
-      erro.push({ phone: "Số điện thoại không được để trống!" });
-    }
-    else {
-      let re = /^(0|\+84)(\s|\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\d)(\s|\.)?(\d{3})(\s|\.)?(\d{3})$/;
-      if (!re.test(String(staff.phone).toLowerCase())) {
-        erro.push({ phone: "Số điện thoại sai cú pháp" })
-      }
-    }
-    return erro;
-  }
-
-  handleOk(frm: any): void {
-    this.newStaff = frm.value;
-    let erro = this.valid(this.newStaff);
-    if (erro.length === 0) {
-      console.log('ok')
-    }
-    else {
-      console.log('fail')
+  handleOk(): void {
+    if (
+      this.style.id === "is-valid" &&
+      this.style.name === "is-valid" &&
+      this.style.email === "is-valid" &&
+      this.style.phone === "is-valid" &&
+      this.style.address === "is-valid"
+    ) {
+      this.listSearch.push({...this.newStaff});
+      console.log(this.listStaff);
+    } else {
+      console.log("errrororooror");
     }
     this.isVisible = false;
   }
 
   handleCancel(): void {
-    console.log('Button cancel clicked!');
+    console.log("Button cancel clicked!");
     this.isVisible = false;
+  }
+  onValid(value): void {
+    // event.target.style={'is-invalid':false,'is-valid':false};
+    // tslint:disable-next-line: no-debugger
+    switch (value.name) {
+      case "id":
+        // tslint:disable-next-line: triple-equals
+        if (value.value == "") {
+          this.style.id = "is-invalid";
+        } else {
+          this.style.id = "is-valid";
+        }
+        break;
+      case "name":
+        // tslint:disable-next-line: triple-equals
+        if (value.value == "") {
+          this.style.name = "is-invalid";
+        } else {
+          this.style.name = "is-valid";
+        }
+        break;
+      case "email":
+        // tslint:disable-next-line: max-line-length
+        const mail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (
+          // tslint:disable-next-line: triple-equals
+          value.value == "" ||
+          !mail.test(String(value.value).toLowerCase())
+        ) {
+          this.style.email = "is-invalid";
+        } else {
+          this.style.email = "is-valid";
+        }
+        break;
+      case "phone":
+        const phone = /^(0|\+84)(\s|\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\d)(\s|\.)?(\d{3})(\s|\.)?(\d{3})$/;
+        if (
+          // tslint:disable-next-line: triple-equals
+          value.value == "" ||
+          !phone.test(String(value.value).toLowerCase())
+        ) {
+          this.style.phone = "is-invalid";
+        } else {
+          this.style.phone = "is-valid";
+        }
+        break;
+      case "address":
+        // tslint:disable-next-line: triple-equals
+        if (value.value == "") {
+          this.style.address = "is-invalid";
+        } else {
+          this.style.address = "is-valid";
+        }
+        break;
+    }
   }
 }
 //https://stackblitz.com/edit/angular-search-filter
